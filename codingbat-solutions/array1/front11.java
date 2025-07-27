ae6390dc76ff35f7e41efb15e194e406d0182040
Az Az <hilal.saleh227@outlook.com>
2025-07-27 20:43:10 +0400

    Add all Array-1 solution files

---
+++ codingbat-solutions/array1/front11.java
@@ -0,0 +1,8 @@
+public class front11 {
+    public int[] front11(int[] a, int[] b) {
+        if (a.length == 0 && b.length == 0) return new int[]{};
+        if (a.length == 0) return new int[]{b[0]};
+        if (b.length == 0) return new int[]{a[0]};
+        return new int[]{a[0], b[0]};
+    }
+}
